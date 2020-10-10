9b82134e045d7bcf6423c587ca101ea5619758d1
jpreciadom <jdiegopm12@gmail.com>
2020-10-10 18:30:48 -0500

    Cuantización y muestreo

--- Lab 2/Ejercicio 1/Ejercicio1.m
+++ Lab 2/Ejercicio 1/Ejercicio1.m
@@ -11,14 +11,52 @@ T = 1/(2*f);
 t = [0 : 0.001 : f];
 
 %Se grafica la onda m(t)
-subplot(2,1,1);
+subplot(2,2,1);
 plot(t, m(t), "-");
 axis([0 f -1.5 1.5]);
 
-%Se crea la función delta para tomar muestras de la onda
-delta = inline(" rem(t, T) <  T/8 ", "t");
+opcion = 2;
 
-%Se grafican las muestras tomadas de la onda, delta(t)*m(t)
-subplot(2,1,2);
-plot(t, m(t).*delta(t));
+%-----------------------------------MUESTREO-----------------------------------%
+
+subplot(2,2,2);
+
+if opcion == 1
+  tn = [0: T : f];
+  stem(tn, m(tn));
+else
+  %Se crea la función delta para tomar muestras de la onda
+  delta = inline(" rem(t, T) <  T/8 ", "t");
+  
+  %Se grafican las muestras tomadas de la onda, delta(t)*m(t)
+  plot(t, m(t).*delta(t));
+end
+
 axis([0 f -1.5 1.5]);
+
+%---------------------------------CUANTIZACION---------------------------------%
+
+%Valor maximo de la onda
+mp = 1;
+
+%Cantidad de zonas, calculadas por la amplitud maxima de la onda
+L = ceil(mp) * 10;
+
+%Tamaño Delta de cada zona
+Delta = (2*mp) / L;
+
+subplot(2,2,3);
+
+if opcion == 1
+  coder = inline(" Delta * floor(m(tn) / Delta) + (Delta/2) ", "t");
+  stem(tn, coder(tn));
+else
+  coder = inline(" Delta * floor(m(t).*delta(t) / Delta) + (Delta/2) ", "t");
+  plot(t, coder(t));
+end
+
+%---------------------------------CODIFICACION---------------------------------%
+
+
+
+%---------------------------FORMATOS DE SEÑALIZACION---------------------------%
