6a89e477bf13816c4afd72efafa815536cfac9ac
jpreciadom <jdiegopm12@gmail.com>
2020-10-10 18:46:57 -0500

    Comentarios

--- Lab 2/Ejercicio 1/Ejercicio1.m
+++ Lab 2/Ejercicio 1/Ejercicio1.m
@@ -2,6 +2,12 @@
 m = input("Ingrese la funcion de la onda m(t): ", 's');
 m = inline(m, "t");
 
+%Valor n que no se para que sirve
+n = 3;
+
+%Valor maximo de la onda
+mp = 1;
+
 %Se solicita la frecuencia de la onda m(t)
 f = input("Indique la frecuencia de la funcion: ");
 %Se calcula el tiempo T que indica cada cuanto se debe toma una muestra de la onda
@@ -15,6 +21,7 @@ subplot(2,2,1);
 plot(t, m(t), "-");
 axis([0 f -1.5 1.5]);
 
+%Opcion para graficar el muestreo y la cuantizacion
 opcion = 2;
 
 %-----------------------------------MUESTREO-----------------------------------%
@@ -22,7 +29,9 @@ opcion = 2;
 subplot(2,2,2);
 
 if opcion == 1
+  %Vector para muestrear la onda m(t) cada T segundos
   tn = [0: T : f];
+  %Se grafica en una grafica de puntos
   stem(tn, m(tn));
 else
   %Se crea la función delta para tomar muestras de la onda
@@ -36,11 +45,8 @@ axis([0 f -1.5 1.5]);
 
 %---------------------------------CUANTIZACION---------------------------------%
 
-%Valor maximo de la onda
-mp = 1;
-
 %Cantidad de zonas, calculadas por la amplitud maxima de la onda
-L = ceil(mp) * 10;
+L = 2 ^ 3;
 
 %Tamaño Delta de cada zona
 Delta = (2*mp) / L;
@@ -48,10 +54,14 @@ Delta = (2*mp) / L;
 subplot(2,2,3);
 
 if opcion == 1
+  %Se crea una funcion coder en base al vector tN
   coder = inline(" Delta * floor(m(tn) / Delta) + (Delta/2) ", "t");
+  %Se grafica en una grafica de puntos
   stem(tn, coder(tn));
 else
+  %Se crea una funcion coder en base a la funcion m(t) y la funcion delta
   coder = inline(" Delta * floor(m(t).*delta(t) / Delta) + (Delta/2) ", "t");
+  %Se grafica la codificacion
   plot(t, coder(t));
 end
 
