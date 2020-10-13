e1697a0de4a943ae81160d2c48bb89b44921c82f
jpreciadom <jdiegopm12@gmail.com>
2020-10-13 18:08:39 -0500

    Falta el ultimo punto

--- Lab 2/Ejercicio 1/Ejercicio1.m
+++ Lab 2/Ejercicio 1/Ejercicio1.m
@@ -79,118 +79,118 @@ codificacion
 %------------------------------------------------------------------------------%
 
 %-------Literal d. REPRESENTACION DEL PULSO-------------------------------
-disp("Seleccione el tipo de codificaciï¿½n :");
-disp("1-Unipolar NRZ");
-disp("2-Bipolar NRZ");
-disp("3-Unipolar RZ");
-disp("4-Bipolar RZ");
-disp("5-AMI");
-disp("6-Manchester");
-opcion=input("ingrese la opción: ");
-y=[];
 
-#Tipos dependiendo de la eleccion
-f_s=100;
-#No Return to Zero
-NRZ=ones(1,f_s);
-#Return Zero
-RZ= [ones(1,f_s/2) zeros(1,f_s/2)];
-#Caso especial para Manchester
-Man=[ones(1,f_s/2)  -ones(1,f_s/2)] ;
+while 1
+ 
+  disp("Seleccione el tipo de codificación :");
+  disp("0-Continuar");
+  disp("1-Unipolar NRZ");
+  disp("2-Bipolar NRZ");
+  disp("3-Unipolar RZ");
+  disp("4-Bipolar RZ");
+  disp("5-AMI");
+  disp("6-Manchester");
+  disp("")
+  opcion=input("ingrese la opción: ");
+  y=[];
 
-switch (opcion)
-	case 1
-		#El nivel de amplitud se mantiene durante todo el intervalo de bit
-    #Solo admite salidas positivas
-		tipo=NRZ;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-		            y=[y  tipo];
-		        case 0
-		            y=[y  (0*tipo)];        
-		    end
-		end
-	case 2
-		#El nivel de amplitud se mantiene durante todo el intervalo de bit
-    #Admite salidas positivas y negativas
-		tipo=NRZ;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-		            y=[y  tipo];
-		        case 0
-		             y=[y  -tipo];
-		    end
-		end
-	case 3
-		#El pulso positivo retorna a cero
-    #Solo admite salidas positivas
-		tipo=RZ;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-		            y=[y  tipo];
-		        case 0
-		            y=[y  (0*tipo)];        
-		    end
-		end
-	case 4
-		#Pulsos con retorno a zero en la mitad del bit
-    #Admite salidas positivas y negativas
-		tipo=RZ;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-		            y=[y  tipo];
-		        case 0
-		             y=[y  -tipo];
-		    end
-		end
-	case 5
-		#Pulsos de 1 se alternan entre positivo y negativo,pulso 0 ausencia de pulso
-		tipo=RZ;
-		count = 0;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-              if mod (count,2) == 0
-                y=[y  tipo];
-              else
-                y=[y  -tipo];		          	
-              endif
-              count++;
-		        case 0
-		            y=[y  (0*tipo)];        
-		    end
-		end		
-	case 6
-		#pulso positivo: inicia positivo termina negativo
-    #pulso negativo: inicia negativo termina positivo 
-		tipo=Man;
-		for i=1:length(y_niveles_binario)
-		    switch y_niveles_binario(i)
-		        case 1
-		            y=[y  tipo];
-		        case 0
-		             y=[y  -tipo];
-		    end
-		end
-endswitch
+  #Tipos dependiendo de la eleccion
+  f_s=100;
+  #No Return to Zero
+  NRZ=ones(1,f_s);
+  #Return Zero
+  RZ= [ones(1,f_s/2) zeros(1,f_s/2)];
+  #Caso especial para Manchester
+  Man=[ones(1,f_s/2)  -ones(1,f_s/2)] ;
 
-  t1=(0:(length(y)-1))/f_s;
-  figure(2);
-  subplot(1,1,1);plot(t1,y,'k');set(gca,'Xlim',[0 8]); set(gca,'XTick',(0:1:8)); axis([0 8 -1.1 1.1]); title('SeÃ±al codificada');xlabel('nT_s'); ylabel('x(nT_s)');
+  switch (opcion)
+    case 0
+      break
+    case 1
+      #El nivel de amplitud se mantiene durante todo el intervalo de bit
+      #Solo admite salidas positivas
+      tipo=NRZ;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                  y=[y  tipo];
+              case 0
+                  y=[y  (0*tipo)];        
+          end
+      end
+    case 2
+      #El nivel de amplitud se mantiene durante todo el intervalo de bit
+      #Admite salidas positivas y negativas
+      tipo=NRZ;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                  y=[y  tipo];
+              case 0
+                   y=[y  -tipo];
+          end
+      end
+    case 3
+      #El pulso positivo retorna a cero
+      #Solo admite salidas positivas
+      tipo=RZ;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                  y=[y  tipo];
+              case 0
+                  y=[y  (0*tipo)];        
+          end
+      end
+    case 4
+      #Pulsos con retorno a zero en la mitad del bit
+      #Admite salidas positivas y negativas
+      tipo=RZ;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                  y=[y  tipo];
+              case 0
+                   y=[y  -tipo];
+          end
+      end
+    case 5
+      #Pulsos de 1 se alternan entre positivo y negativo,pulso 0 ausencia de pulso
+      tipo=RZ;
+      count = 0;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                if mod (count,2) == 0
+                  y=[y  tipo];
+                else
+                  y=[y  -tipo];		          	
+                endif
+                count++;
+              case 0
+                  y=[y  (0*tipo)];        
+          end
+      end		
+    case 6
+      #pulso positivo: inicia positivo termina negativo
+      #pulso negativo: inicia negativo termina positivo 
+      tipo=Man;
+      for i=1:length(y_niveles_binario)
+          switch y_niveles_binario(i)
+              case 1
+                  y=[y  tipo];
+              case 0
+                   y=[y  -tipo];
+          end
+      end
+  endswitch
+
+    t1=(0:(length(y)-1))/f_s;
+    subplot(2,2,4);
+    plot(t1,y,'k');set(gca,'Xlim',[0 8]); set(gca,'XTick',(0:1:8));
+    axis([0 8 -1.1 1.1]); title('SeÃ±al codificada');xlabel('nT_s'); ylabel('x(nT_s)');
+    disp(""); disp(""); disp(""); disp(""); disp(""); disp("");
+endwhile
   
   %-----------------Literal e. RECUPERAR M(T)----------------
-
-sum=0;
-w_m=f;
-for i=0:100
-  fun=m(i*T)*(sin(w_m.*(t-i*T))/w_m.*(t-i*T));
-  sum=sum+fun;
-endfor
-figure(3);
-plot(t,sum); title('Recuperacion de la seÃ±al');
-
 
