600ffc62b0e6edb565697ff716a2898721b0ac70
jpreciadom <jdiegopm12@gmail.com>
2020-12-03 18:30:27 -0500

    Lab4 - 1 Codificacion de un mensaje

--- Lab 4/Ejercicio1/ejercicio1.m
+++ Lab 4/Ejercicio1/ejercicio1.m
@@ -4,7 +4,16 @@ n = 6;
 k = 3;
 
 % Matriz generadora G
-G = [1 0 0 1 1 1; 0 1 0 0 1 1; 0 0 1 1 1 0];
+G = [1 0 0 1 1 1;
+     0 1 0 0 1 1;
+     0 0 1 1 1 0];
+% Matriz de chequeo de paridad Ht
+Ht = [1 1 1;
+      0 1 1;
+      1 1 0;
+      1 0 0;
+      0 1 0;
+      0 0 1]
 disp(newline);
 
 % Se crea una tabla vacia para almacenar las palabras y su codificación
@@ -26,3 +35,42 @@ T.("c") = cod;
 
 % Se imprime la tabla
 disp(T);
+
+% Ciclo para solicitar al usuario palabras para codificar o decodificar
+opcion = 3;
+while(opcion ~= 0)
+    % Se pregunta al usuario lo que desea hacer
+    disp(newline);
+    disp("Elija una opción: ");
+    disp("1. Codificar un mensaje");
+    disp("2. Decodificar un mensaje");
+    disp("0. Terminar ejecición")
+    opcion = input("Opción: ");
+    disp(newline);
+    %If para mirar la opcion ingresada
+    if(opcion == 1)
+        TC = table;
+        mensaje = input("Ingrese el mensaje binario que desea codificar: ", "s");
+        disp(newline);
+        mSize = size(mensaje, 2);
+        bloques = mSize/k;
+        
+        indices = zeros(bloques,1);
+        m = {bloques};
+        for i = 1 : bloques
+            indices(i, 1) = i;
+            im = "";
+            for j = 1:k
+                im = im + mensaje((i*k)-k+j);
+            end
+            m(1,i) = cellstr(im);
+        end
+        m = char(string(m)');
+        TC.("i") = indices;
+        TC.("bloque_i") = m;
+        TC.("c_i") = codificar(m, G);
+        disp(TC);
+    elseif(opcion == 2)
+        mensaje = input("Ingrese el mensaje que desea decodificar: ");
+    end
+end
